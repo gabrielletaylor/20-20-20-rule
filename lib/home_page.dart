deab0206ffb9fbb53c9eb7841170ce76449efb72
Gabrielle David <gtdavid@cpp.edu>
2022-12-03 02:30:59 -0800

    Change timer format and fix timer bug

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,41 +11,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Timer? countdownTimer;
+  Duration duration = Duration(days: 5);
+  final reduceSecondsBy = 1;
   double percent = 0;
   static int timeInMinute = 20;
   int timeInSec = timeInMinute * 60;
-  late Timer timer;
+  double secPercentage = 0;
+  bool startPressed = false;
 
-  _startTimer() {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void startTimer() {
+    // if (!countdownTimer!.isActive) {
+    startPressed = true;
     timeInMinute = 20;
-    double secPercentage = (timeInSec / 100);
-    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        if (timeInSec > 0) {
-          timeInSec--;
-          if (timeInSec % 60 == 0) {
-            timeInMinute--;
-          }
+    secPercentage = (timeInSec / 100);
+    countdownTimer = Timer.periodic(Duration(seconds: 1), (_) => setCountdown());
+    // }
+  }
 
-          if (timeInSec % secPercentage == 0) {
-            if (percent < 1) {
-              percent += 0.01;
-            } else {
-              percent = 1;
-            }
-          }
-        } else {
-          percent = 0;
-          timeInMinute = 20;
-          timeInSec = timeInMinute * 60;
-          timer.cancel();
+  void pauseTimer() {
+    setState(() => countdownTimer!.cancel());
+    startPressed = false;
+  }
+
+  void resetTimer() {
+    pauseTimer();
+    percent = 0;
+    timeInMinute = 20;
+    timeInSec = timeInMinute * 60;
+    setState(() => duration = Duration(days: 5));
+  }
+
+  void setCountdown() {
+    setState(() {
+      final seconds = duration.inSeconds - reduceSecondsBy;
+      if (seconds < 0) {
+        percent = 0;
+        timeInMinute = 20;
+        timeInSec = timeInMinute * 60;
+        countdownTimer!.cancel();
+      }
+      else {
+        duration = Duration(seconds: seconds);
+        timeInSec--;
+        if (timeInSec % 60 == 0) {
+          timeInMinute--;
         }
-      });
+
+        if (timeInSec % secPercentage == 0) {
+          if (percent < 1) {
+            percent += 0.01;
+          } else {
+            percent = 1;
+          }
+        }
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    String strDigits(int n) => n.toString().padLeft(2, '0');
+    final days = strDigits(duration.inDays);
+    final minutes = strDigits(duration.inMinutes.remainder(20));
+    final seconds = strDigits(duration.inSeconds.remainder(60));
+
     return SafeArea(
       child: Scaffold(
         body: Container(
@@ -65,13 +100,14 @@ class _HomePageState extends State<HomePage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Padding(
-                padding: EdgeInsets.only(top: 24.0),
+                padding: EdgeInsets.only(top: 40.0),
                 child: Text(
                   '20-20-20 Rule',
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 36,
-                      fontFamily: 'RobotoMono'),
+                      fontFamily: 'RobotoMono'
+                  ),
                 ),
               ),
               Expanded(
@@ -80,20 +116,19 @@ class _HomePageState extends State<HomePage> {
                   percent: percent,
                   animation: true,
                   animateFromLastPercent: true,
-                  radius: 100.0,
+                  radius: 150.0,
                   lineWidth: 16.0,
                   progressColor: Colors.amber,
                   center: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        '$timeInSec',
+                        '$minutes:$seconds',
                         style: const TextStyle(
                           color: Colors.white,
                           fontSize: 50,
                         ),
                       ),
-                      const Text('seconds'),
                     ],
                   ),
                 ),
@@ -144,7 +179,7 @@ class _HomePageState extends State<HomePage> {
                                 child: Column(
                                   children: const [
                                     Text(
-                                      'Pause Time',
+                                      'Rest Time',
                                       style: TextStyle(fontSize: 20.0),
                                     ),
                                     SizedBox(
@@ -164,29 +199,92 @@ class _HomePageState extends State<HomePage> {
                             ],
                           ),
                         ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 40.0),
-                            child: ElevatedButton(
-                              child: const Padding(
-                                padding: EdgeInsets.all(20.0),
-                                child: Text(
-                                  'Start Timer',
-                                  style: TextStyle(
-                                    color: Colors.white,
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 40.0),
+                                child: ElevatedButton(
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(20.0),
+                                    child: Text(
+                                      'Start Timer',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    if (!startPressed) {
+                                      startTimer();
+                                    }
+                                    // startTimer();
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    elevation: 4,
+                                    primary: Colors.blue.shade700,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(100.0),
+                                    ),
                                   ),
                                 ),
                               ),
-                              onPressed: _startTimer,
-                              style: ElevatedButton.styleFrom(
-                                elevation: 4,
-                                primary: Colors.blue.shade700,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(100.0),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 40.0),
+                                child: ElevatedButton(
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(20.0),
+                                    child: Text(
+                                      'Pause Timer',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    if (countdownTimer == null || countdownTimer!.isActive) {
+                                      pauseTimer();
+                                    }
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    elevation: 4,
+                                    primary: Colors.blue.shade700,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(100.0),
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 40.0),
+                                child: ElevatedButton(
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(20.0),
+                                    child: Text(
+                                      'Reset Timer',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    resetTimer();
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    elevation: 4,
+                                    primary: Colors.blue.shade700,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(100.0),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
@@ -200,3 +298,113 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+// class _HomePageState extends State<HomePage> {
+//   // Step 2
+//   Timer? countdownTimer;
+//   Duration myDuration = Duration(days: 5);
+//   bool startPressed = false;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+//   /// Timer related methods ///
+//   // Step 3
+//   void startTimer() {
+//     print(startPressed);
+//     if (startPressed) {
+//       countdownTimer =
+//           Timer.periodic(Duration(seconds: 1), (_) => setCountDown());
+//     }
+//   }
+//   // Step 4
+//   void stopTimer() {
+//     setState(() => countdownTimer!.cancel());
+//   }
+//   // Step 5
+//   void resetTimer() {
+//     stopTimer();
+//     setState(() => myDuration = Duration(days: 5));
+//   }
+//   // Step 6
+//   void setCountDown() {
+//     final reduceSecondsBy = 1;
+//     setState(() {
+//       final seconds = myDuration.inSeconds - reduceSecondsBy;
+//       if (seconds < 0) {
+//         countdownTimer!.cancel();
+//       } else {
+//         myDuration = Duration(seconds: seconds);
+//       }
+//     });
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     String strDigits(int n) => n.toString().padLeft(2, '0');
+//     final days = strDigits(myDuration.inDays);
+//     // Step 7
+//     // final hours = strDigits(myDuration.inHours.remainder(24));
+//     final minutes = strDigits(myDuration.inMinutes.remainder(20));
+//     final seconds = strDigits(myDuration.inSeconds.remainder(60));
+//     return Scaffold(
+//       // appBar: ...,
+//       body: Center(
+//         child: Column(
+//           children: [
+//             SizedBox(
+//               height: 50,
+//             ),
+//             // Step 8
+//             Text(
+//               '$minutes:$seconds',
+//               style: TextStyle(
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.black,
+//                   fontSize: 50),
+//             ),
+//             SizedBox(height: 20),
+//             // Step 9
+//             ElevatedButton(
+//               onPressed: () {
+//                 startPressed = true;
+//                 startTimer();
+//               },
+//               child: Text(
+//                 'Start',
+//                 style: TextStyle(
+//                   fontSize: 30,
+//                 ),
+//               ),
+//             ),
+//             // Step 10
+//             ElevatedButton(
+//               onPressed: () {
+//                 if (countdownTimer == null || countdownTimer!.isActive) {
+//                   stopTimer();
+//                 }
+//               },
+//               child: Text(
+//                 'Stop',
+//                 style: TextStyle(
+//                   fontSize: 30,
+//                 ),
+//               ),
+//             ),
+//             // Step 11
+//             ElevatedButton(
+//                 onPressed: () {
+//                   resetTimer();
+//                 },
+//                 child: Text(
+//                   'Reset',
+//                   style: TextStyle(
+//                     fontSize: 30,
+//                   ),
+//                 ))
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
